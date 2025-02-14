9505cdb8e29ec2a4670412dfde9b0c0936e065fe
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-02-14 22:20:12 +0530

    Partial Commit

---
+++ src/test/java/testCases/RemindClass.java
@@ -0,0 +1,21 @@
+package testCases;
+
+public class RemindClass {
+
+	/*
+	 * -------Below Test Scenarios are Valid under Manual Testing--------- 
+	 * Validate the working of 'My Orders' functionality 
+	 * Validate the working of 'Shopping Cart' functionality 
+	 * Validate the working of Checkout Page functionality with Sign-in to Application 
+	 * Validate the working of Checkout Page functionality As a Guest User 
+	 * Validate the working of Home Page functionality Validate the working of My Account functionality 
+	 * Validate the working of My Account > 'My Account Information' Page functionality 
+	 * Validate the working of My Account > 'Address Book' Page functionality 
+	 * Validate the working of My Account > 'My Product Review' Page functionality 
+	 * Validate the working of 'Sales' Page Functionality 
+	 * Validate the working of 'What's New' Page Functionality
+	 * Validate the working of Advanced Search Functionality 
+	 * Validate the working of Header, Menu and Footer functionality
+	 */
+
+}
