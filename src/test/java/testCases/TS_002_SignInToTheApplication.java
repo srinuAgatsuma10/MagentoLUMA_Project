b677cd455602e30207ac9821477a78df163e960a
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-02-15 18:46:05 +0530

    Partial Commit

--- src/test/java/testCases/TS_002_SignInToTheApplication.java
+++ src/test/java/testCases/TS_002_SignInToTheApplication.java
@@ -12,34 +12,33 @@ public class TS_002_SignInToTheApplication extends TestCaseBaseClass {
 	
 	@Test(groups = {"Sanity","Functional","Master"})
 	public void signInApplication() {
-		logger.info("****Starting TS_002_SignInToTheApplication ****");
+		
 		try
 		{
-			logger.info("Click on SignIn button");
+		
 		HomePagePOM hp = new HomePagePOM(driver);
 		hp.clickSignIn();
 		
-		logger.info("Enter Valid credentials");
+		
 		SignInPOM sp = new SignInPOM(driver);;
 		sp.emailInputBox(prop.getProperty("workingEmail1"));
 		sp.passwordInputBox(prop.getProperty("we1Password"));
 		
-		logger.info("Click on SignButton");
+		
 		sp.signInbtn();
 		
 //		sp.forgotPasswordLink();
 //		sp.btn_CreateNewAcc();
 		
-		logger.info("Validate Test Case");
+		
 		Boolean greetings = sp.getWlcomeMessage();		
 		Assert.assertTrue(greetings);
 		}
 		catch(Exception e) {
-			logger.error("Test Failed");
-			logger.debug("Debug code");
+		
 			Assert.fail();
 		}
-		logger.info("****Finishing TS_002_SignInToTheApplication ****");
+		logger.info("Test finished");
 	}
 	
 
