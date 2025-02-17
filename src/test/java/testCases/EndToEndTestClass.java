2d107093c4df58363f09c3a2e6b69b7e98350698
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-02-17 22:12:13 +0530

    The Commit

--- src/test/java/testCases/EndToEndTestClass.java
+++ src/test/java/testCases/EndToEndTestClass.java
@@ -1,5 +1,31 @@
 package testCases;
 
-public class EndToEndTestClass {
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
+import testBase.TestCaseBaseClass;
+
+
+/**
+ * Test Case: End-to-End test
+ * 
+ * Steps:
+ * 1) Perform Account Registration
+ * 2) Logout after successful registration
+ * 3) User Login with the registered email
+ * 4) Search product and add to cart
+ * 5) Verify Shopping Cart contents
+ * 6) Perform Checkout process
+ */
+
+
+public class EndToEndTestClass extends TestCaseBaseClass {
+
+	@Test
+	public void endToEndTest() {
+		 SoftAssert sa = new SoftAssert();
+
+		 
+	}
+	
 }
